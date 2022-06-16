551bf039f58f2c79c0577f02ba173a93a81c40b9
Brian Ma <chma0@nuvoton.com>
2022-06-16 17:45:57 +0800

    Update Readme: Add BIOS version part
    Signed-off-by: Brian Ma <chma0@nuvoton.com>

--- script/version_verify.py
+++ script/version_verify.py
@@ -3,6 +3,9 @@
 # Example: 
 #   python ./version_verify.py /tmp/data
 #   echo "14 43 32 31 39 35 2e" | python ./version_verify.py -s
+#   equals: echo "14 43 32 31 39 35 2e" | xxd -r -p
+#   Or convert version string to ipmid format:
+#   python ./version_verify.py -r C2195.0.BS.1A06.GN.3
 
 import os
 import sys
@@ -24,6 +27,23 @@ def convert_byte_file(file, length_byte):
         data = f.readlines()
         return convert_byte(data, length_byte)
 
+def print_hex(ver, length_byte):
+    # insert length byte if need
+    if length_byte:
+        ver = bytes([len(ver)]).decode('utf-8') + ver
+    # convert string to hex format, and split each byte by space
+    data = " ".join("{:02x}".format(ord(c)) for c in ver)
+    # print new line for each 16 bytes to match format as ipmid
+    data_bytes = data.split(" ")
+    lines = []
+    index = 0
+    while (index < len(data_bytes)):
+        data_16 = data_bytes[index:index+16]
+        if (len(data_16) > 0):
+            lines.append(" ".join(str(data_byte) for data_byte in data_16))
+        index += 16
+
+    return "\n".join(lines)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -34,11 +54,15 @@ def main():
     )
 
     parser.add_argument(
-        '-s', '--stdin',  dest='stdin', action="store_true",
+        '-s', '--stdin', dest='stdin', action="store_true",
         help="read data from stdin instead of file"
     )
     parser.add_argument(
-        '-v', '--verbose',  dest='verb', action="store_true",
+        '-r', '--revert', dest='revert', action="store_true",
+        help="convert version string to bytes"
+    )
+    parser.add_argument(
+        '-v', '--verbose', dest='verb', action="store_true",
         help="show verbose messages"
     )
 
@@ -46,15 +70,23 @@ def main():
 
     # dump arguments
     global VERBOSE
+    byte_count = True
     if args.verb:
         print("input file     : {}".format(args.file))
+        print("process length : {}".format(byte_count))
         VERBOSE = args.verb
     if args.stdin:
-        ver = convert_byte_stdin(True)
+        ver = convert_byte_stdin(byte_count)
+    elif args.revert:
+        if (len(args.file) == 0):
+            print("Input string should not be empty!!\n")
+            # parser.print_help()
+            return
+        ver = print_hex(args.file, byte_count)
     else:
         if (not (os.path.isfile(args.file))):
             sys.exit("Can not find input file " + args.file)
-        ver = convert_byte_file(args.file, True)
+        ver = convert_byte_file(args.file, byte_count)
 
     print(ver)
 
